b3aa565130cbb455efa3117d40efb9c8b3b9989a
Matiduraan <63005534+Matiduraan@users.noreply.github.com>
2020-05-15 11:42:00 -0300

    Nueva entrega

--- TPMonopoly_MatiasDuran.hs
+++ TPMonopoly_MatiasDuran.hs
@@ -1,6 +1,3 @@
-module Library where
-import Data.List
-import Data.Char
 import Text.Show.Functions()
 
 type Propiedad = (String,Int)
@@ -22,44 +19,64 @@ carolina = UnParticipante "Carolina" 500 "Accionista" [] [pasarPorElBanco, pagar
 manuel :: Participante
 manuel = UnParticipante "Manuel" 500 "Oferente singular" [] [pasarPorElBanco, enojarse]
 
-pasarPorElBanco :: Accion
-pasarPorElBanco unParticipante = unParticipante { dinero = dinero unParticipante + 40, tactica = "Comprador Compulsivo", acciones = acciones unParticipante ++ [pasarPorElBanco]}
+puedeComprarPropiedad:: Propiedad -> Participante -> Bool
+puedeComprarPropiedad unaPropiedad unParticipante = dinero unParticipante >= (obtenerPrecio unaPropiedad)
 
-agregarAccion :: Participante -> Accion -> Participante
-agregarAccion unParticipante accion = unParticipante {acciones = acciones unParticipante ++ [accion] }
+cambiarTactica :: String -> Participante -> Participante
+cambiarTactica unaTactica unParticipante = unParticipante {tactica = unaTactica}
+
+obtenerPrecio:: Propiedad -> Int
+obtenerPrecio (_,precio) = precio
+
+agregarDinero :: Int -> Accion
+agregarDinero monto unParticipante = unParticipante {dinero = dinero unParticipante + monto}
+
+pasarPorElBanco :: Accion
+pasarPorElBanco unParticipante = (agregarDinero 40 . cambiarTactica "Comprador Compulsivo") unParticipante
 
 enojarse:: Accion
-enojarse unParticipante = gritar (unParticipante {dinero = dinero unParticipante + 50, acciones = acciones unParticipante ++ [enojarse]})
+enojarse unParticipante = gritar (agregarDinero 50 unParticipante)
 
 gritar:: Accion
-gritar unParticipante =  unParticipante {nombre = "AHHHH " ++ nombre unParticipante, acciones = acciones unParticipante ++ [gritar]}
+gritar unParticipante =  unParticipante {nombre = "AHHHH " ++ nombre unParticipante}
 
-cobrarAlquileres unParticipante = unParticipante {dinero = (dinero unParticipante + (10 * (propiedadesBaratas (propiedades unParticipante))) + (20 * (propiedadesCaras (propiedades unParticipante)))), acciones = acciones unParticipante ++ [cobrarAlquileres]}
+cobrarAlquileres :: Accion
+cobrarAlquileres unParticipante = agregarDinero ((10 * (propiedadesBaratas (propiedades unParticipante))) + (20 * (propiedadesCaras (propiedades unParticipante)))) unParticipante 
 
 
 propiedadesCaras:: [Propiedad] -> Int
-propiedadesCaras props = length(filter (>=150) (map (obtenerValor) props))
+propiedadesCaras props = (length.(filter (>=150)).(map (obtenerPrecio))) props
 propiedadesBaratas:: [Propiedad] -> Int
-propiedadesBaratas props = length(filter (<150) (map (obtenerValor) props))
-
-obtenerValor:: Propiedad -> Int
-obtenerValor (_,valor) = valor
+propiedadesBaratas props = length(filter (<150) (map (obtenerPrecio) props))
 
 pagarAAccionistas :: Accion
 pagarAAccionistas unParticipante 
-    | esAccionista unParticipante = unParticipante {dinero = dinero unParticipante + 200, acciones = acciones unParticipante ++ [pagarAAccionistas]}
-    | otherwise                   = unParticipante {dinero = dinero unParticipante - 100, acciones = acciones unParticipante ++ [pagarAAccionistas]}
-
-esAccionista :: Participante -> Bool
-esAccionista unParticipante = tactica unParticipante == "Accionista"
+    | tieneTactica "Accionista" unParticipante = agregarDinero 200 unParticipante
+    | otherwise                                = agregarDinero (-100) unParticipante
 
 tieneTactica :: String -> Participante -> Bool
 tieneTactica unaTactica unParticipante = tactica unParticipante == unaTactica 
 
 subastar:: Participante -> Propiedad -> Participante
 subastar unParticipante unaPropiedad
-      | tieneTactica "Oferente Singular" unParticipante = unParticipante {dinero = dinero unParticipante - (obtenerValor unaPropiedad), propiedades = propiedades unParticipante ++ [unaPropiedad]}
-      | tieneTactica "Accionista" unParticipante = unParticipante {dinero = dinero unParticipante - (obtenerValor unaPropiedad), propiedades = propiedades unParticipante ++ [unaPropiedad]}
+      | tieneTactica "Oferente Singular" unParticipante || tieneTactica "Accionista" unParticipante = comprarPropiedad unaPropiedad unParticipante
       | otherwise = unParticipante
 
+comprarPropiedad:: Propiedad -> Accion
+comprarPropiedad unaPropiedad unParticipante = unParticipante{dinero = dinero unParticipante - (obtenerPrecio unaPropiedad), propiedades = propiedades unParticipante ++ [unaPropiedad]}
 
+hacerBerrinchePor:: Propiedad -> Participante -> Participante
+hacerBerrinchePor unaPropiedad unParticipante
+      | puedeComprarPropiedad unaPropiedad unParticipante = (agregarDinero (obtenerPrecio unaPropiedad) . comprarPropiedad unaPropiedad) unParticipante
+      | otherwise                                          = (hacerBerrinchePor unaPropiedad . agregarDinero 10) unParticipante
+
+ultimaRonda :: Participante -> Participante -> Participante
+ultimaRonda unParticipante = foldl1 (.) (acciones unParticipante)
+
+juegoFinal :: Participante -> Participante -> Participante
+juegoFinal primerParticipante segundoParticipante
+      |  dineroEnLaUltimaRonda primerParticipante >= dineroEnLaUltimaRonda segundoParticipante = primerParticipante
+      |  otherwise                                                                             = segundoParticipante
+
+dineroEnLaUltimaRonda :: Participante -> Int
+dineroEnLaUltimaRonda unParticipante = dinero ((ultimaRonda unParticipante) unParticipante)
